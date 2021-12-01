30712fe2f95cd38ac9ead1f027d3dde40d82b5b6
Shivek Khurana <shivek@status.im>
2021-12-01 17:48:44 +0530

    Selector events

--- src/status_im/ui/screens/wallet/swap/views.cljs
+++ src/status_im/ui/screens/wallet/swap/views.cljs
@@ -3,26 +3,52 @@
             [quo.design-system.colors :as colors]
             [re-frame.core :as re-frame]
             [status-im.ethereum.tokens :as tokens]
+            [status-im.i18n.i18n :as i18n]
+            [status-im.ui.components.chat-icon.screen :as chat-icon]
             [status-im.ui.components.icons.icons :as icons]
             [status-im.ui.components.keyboard-avoid-presentation
              :as
              kb-presentation]
             [status-im.ui.components.react :as react]
+            [status-im.ui.components.search-input.view :as search-input]
             [status-im.ui.components.slider :as slider]
+            [status-im.wallet.swap.core :as wallet.swap]
             [status-im.ui.components.toolbar :as toolbar]
             [status-im.ui.components.topbar :as topbar]
-            [status-im.ui.screens.wallet.accounts.views :as accounts]
+            [status-im.ui.screens.wallet.components.views :as wallet.components]
             [status-im.utils.handlers :refer [<sub]]
-            [status-im.i18n.i18n :as i18n]
-            [status-im.ui.components.search-input.view :as search-input]))
+            [status-im.wallet.utils :as wallet.utils]))
+
+(defn render-asset [{{:keys
+                      [icon decimals amount color value]
+                      :as token} :token
+                     currency    :currency
+                     on-press    :on-press}]
+  [quo/list-item
+   {:title               [quo/text {:weight :medium}
+                          [quo/text {:weight :inherit}
+                           (str (if amount
+                                  (wallet.utils/format-amount amount decimals)
+                                  "...")
+                                " ")]
+                          [quo/text {:color  :secondary
+                                     :weight :inherit}
+                           (wallet.utils/display-symbol token)]]
+    :on-press            on-press
+    :subtitle            (str (if value value "...") " " currency)
+    :accessibility-label (str (:symbol token) "-asset-value")
+    :icon                (if icon
+                           [wallet.components/token-icon icon]
+                           [chat-icon/custom-icon-view-list (:name token) color])}])
 
 (defn asset-selector []
   (let [{:keys [address]} (<sub [:multiaccount/current-account])
         {:keys [tokens]}  (<sub [:wallet/visible-assets-with-values address])
+        source?           (<sub [:wallet/modal-selecting-source-token?])
         currency          (<sub [:wallet/currency])]
     [:<>
      [topbar/topbar
-      {:title  (if true
+      {:title  (if source?
                  (i18n/label :t/select-token-to-swap)
                  (i18n/label :t/select-token-to-receive))
        :modal? true}]
@@ -33,14 +59,20 @@
      [react/scroll-view
       (for [token tokens]
         ^{:key (:name token)}
-        [accounts/render-asset token nil nil (:code currency)])]]))
+        [render-asset {:token    token
+                       :on-press #(re-frame/dispatch
+                                   [(if source?
+                                      ::wallet.swap/set-from-token
+                                      ::wallet.swap/set-to-token)
+                                    (:symbol token)])
+                       :currency (:code currency)}])]]))
 
 (defn token-display
-  "Show token and act as an anchor to open selector"
-  [token]
+  "Show token and act as an anchor to open selector."
+  [{:keys [token source?]}]
   (let [token-icon-source (-> token :icon :source)]
     [react/touchable-highlight
-     {:on-press #(re-frame/dispatch [:open-modal :swap-asset-selector])}
+     {:on-press #(re-frame/dispatch [::wallet.swap/open-asset-selector-modal source?])}
      [react/view {:style {:flex-direction     :row
                           :align-items        :center
                           :border-width       1
@@ -59,7 +91,7 @@
 
 (defn token-input
   "Component to get the amount and type of tokens"
-  [{:keys [amount error label token max-from]}]
+  [{:keys [amount error label token max-from source?]}]
   (let [window-width (<sub [:dimensions/window-width])]
     [react/view {:style {:justify-content :space-between
                          :flex-direction  :row
@@ -87,7 +119,8 @@
         :auto-focus          true
         :on-change-text      #()
         :placeholder         "0.0"}]]
-     [token-display token]]))
+     [token-display {:token   token
+                     :source? source?}]]))
 
 (defn separator-with-icon []
   [react/view {:margin-vertical 8}
@@ -111,15 +144,13 @@
   (let [{:keys [name]}
         (<sub [:multiaccount/current-account])
 
-        amount     "0.02"
-        from-token (-> tokens/all-tokens-normalized
-                       :mainnet
-                       vals
-                       first)
-        to-token   (-> tokens/all-tokens-normalized
-                       :mainnet
-                       vals
-                       second)]
+        all-tokens  (<sub [:wallet/all-tokens]) 
+        from-symbol (<sub [:wallet/swap-from-token])
+        to-symbol   (<sub [:wallet/swap-to-token])
+        amount      "0.02"
+        from-token  (tokens/symbol->token all-tokens (or from-symbol :DGX))
+        to-token    (tokens/symbol->token all-tokens (or to-symbol :SNT))]
+
     [kb-presentation/keyboard-avoiding-view {:style         {:flex            1
                                                              :justify-content :space-between}
                                              :ignore-offset true}
@@ -134,14 +165,16 @@
                     :error    nil
                     :label    "Amount"
                     :token    from-token
+                    :source?  true
                     :max-from 67.28}]
 
       [separator-with-icon]
 
-      [token-input {:amount "0.01"
-                    :error  nil
-                    :label  "Minimum Received"
-                    :token  to-token}]]
+      [token-input {:amount  "0.01"
+                    :error   nil
+                    :label   "Minimum Received"
+                    :source? false
+                    :token   to-token}]]
 
 
      [slider/animated-slider
--- src/status_im/wallet/swap/core.cljs
+++ src/status_im/wallet/swap/core.cljs
@@ -1,2 +1,42 @@
-(ns status-im.wallet.swap.core)
+(ns status-im.wallet.swap.core
+  (:require [status-im.utils.fx :as fx]
+            [re-frame.db :as re-frame.db]
+            [status-im.navigation :as navigation]
+            [status-im.ethereum.tokens :as tokens]))
+
+(fx/defn open-asset-selector-modal
+  "source? true signinfies we are selecting the source asset. false implies selection of sink asset"
+  {:events [::open-asset-selector-modal]}
+  [{:keys [db]} source?]
+  (fx/merge {:db (assoc db :wallet/modal-selecting-source-token? source?)}
+            (navigation/open-modal :swap-asset-selector {})))
+
+(fx/defn set-from-token
+  {:events [::set-from-token]}
+  [{:keys [db]} from-symbol]
+  (fx/merge {:db (assoc db :wallet/swap-from-token from-symbol)}
+            (navigation/navigate-back)))
+
+(fx/defn set-to-token
+  {:events [::set-to-token]}
+  [{:keys [db]} to-symbol]
+  (fx/merge {:db (assoc db :wallet/swap-to-token to-symbol)}
+            (navigation/navigate-back)))
+
+(fx/defn set-from-token-amount
+  [{:keys [db]} from-amount]
+  {:db (assoc db :wallet/swap-from-token-amount from-amount)})
+
+(fx/defn set-max-from-token-amount
+  [{:keys [db]} _]
+  {:db (assoc db :wallet/swap-from-token-amount 0)})
+
+(comment
+  (->> re-frame.db/app-db
+       deref
+      :wallet/all-tokens
+      vals
+      (map #(str (:name %) "-" (:symbol %)))
+      )
+  )
 
